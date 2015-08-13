98d41a2954cd1f4e279febe3f261130ee8453fe4
moe333sippi <msidani@cinemoz.com>
2015-08-13 13:44:31 +0300

    deleted unneeded variables

--- src/gulp.config.js
+++ src/gulp.config.js
@@ -1,6 +1,5 @@
 module.exports = function () {
 	var config = {
-
 		allcss: "./development/app/_public/styles/css/*.css",
 
 		allfonts: "./app/_public/styles/fonts/*.*",
@@ -15,43 +14,13 @@ module.exports = function () {
 
 		allts: ["./app/app.*.ts", "./app/**/*.ts"],
 
-		browserSync: ["!./app/_public/styles/less/*.less", "./app/**/*.*"],
-
-		build: "./build/",
-
-		buildCss: "./build/app/main.css",
-
-		buildDest: "./build/app",
-
-		buildJs: "./build/app/build.js",
-
-		buildMinJs: "./build/app/build.optimized.min.js",
-
-		buildMinCss: "./build/app/main.optimized.min.css",
-
-		buildTmpl: "./build/app/templates.js",
+		build: "./build/app/",
 
 		client: "./app",
 
-		dev: "./development/",
+		dev: "./development/app/",
 
-		devDest: "./development/app",
-
-		devDestCss: "./development/app/_public/styles/css",
-
-		devMainCss: "./development/app/main.css",
-
-		fontDest: "./build/app/_public/styles/fonts",
-
-		imgDest: "./build/app/_public/img",
-
-		index: "./index.html",
-
-		templates: "./build/app/templates.js",
-
-		watchTS: "./app/**/",
-
-		watchLess: "./app/**/"
+		index: "./index.html"
 	};
 	return config;
 };
