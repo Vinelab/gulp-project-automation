b5d5daa18a602a6cc271b1372494eb11db457ca2
moe333sippi <msidani@cinemoz.com>
2015-08-26 20:00:32 +0300

    Deleted

--- Test/lib/file.ts
+++
@@ -1,6 +0,0 @@
-///<reference path="../../tools/typings/tsd.d.ts" />
-///<reference path="../../tools/typings/typescriptApp.d.ts" />
-
-((): void => {
-    angular.module('demoApp', ['ngRoute', 'ngAnimate']);
-})();
