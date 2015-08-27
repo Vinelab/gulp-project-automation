6c19de46aa8a134e7f4c40868ce748eaa43529f5
moe333sippi <msidani@cinemoz.com>
2015-08-27 17:33:29 +0300

    Added test for dependency injection ngAnnotate

--- gulpfile.js
+++ gulpfile.js
@@ -81,7 +81,7 @@ gulp.task("test-ts-compiler", function () {
                         // Specify ECMAScript target version: 'ES3' (default), or 'ES5' 
                         target : "ES5"
                       }))
-                      .pipe(gulp.dest("./Test/dest"));
+                      .pipe(gulp.dest(config.testDest));
 });
 
 
@@ -97,9 +97,9 @@ gulp.task("less-css", function () {
 * * *This task is used for testing. Compile simple less file to css
 */
 gulp.task("test-less-css", function () {
-    return gulp.src("./Test/lib/*.less")
+    return gulp.src(config.testLib + "*.less")
                .pipe(lazy.less())
-               .pipe(gulp.dest("./Test/dest"));
+               .pipe(gulp.dest(config.testDest));
 });
 
 
@@ -115,9 +115,9 @@ gulp.task("concat-css", function () {
 * * *This task is used for testing. Concat css files
 */
 gulp.task("test-concat-css", function () {
-    return gulp.src(["./Test/dest/test-style*.css", "./Test/dest/newstyle.css"])
+    return gulp.src([config.testDest + "test-style*.css", config.testDest + "newstyle.css"])
                .pipe(lazy.concatCss("test-main.css"))
-               .pipe(gulp.dest("./Test/dest/dest/"));
+               .pipe(gulp.dest(config.testDest + "dest/"));
 });
 
 
@@ -136,12 +136,12 @@ gulp.task("auto-prefixer", function () {
 * * * This task is used for testing. Simply adds browser prefixes to the main css file test-main.css
 */
 gulp.task("test-auto-prefixer", function () {
-    return gulp.src("./Test/dest/dest/test-main.css")
+    return gulp.src(config.testDest + "dest/test-main.css")
                .pipe(lazy.autoprefixer({
                   browsers: ["> 0%"],
                   cascade: true
                }))
-               .pipe(gulp.dest("./Test/dest/dest/"));
+               .pipe(gulp.dest(config.testDest + "dest/"));
 });
 
 
@@ -160,7 +160,7 @@ gulp.task("test-bower-injector", function () {
     return gulp.src(config.index)
                .pipe(wiredep.stream())
                .pipe(lazy.rename({prefix: 'test-'}))
-               .pipe(gulp.dest("./Test/dest/dest"));
+               .pipe(gulp.dest(config.testDest + "dest/"));
 });
 
 
@@ -176,9 +176,9 @@ gulp.task("js-injector", function () {
 * * * This task is used for testing. Simply adds js scripts components into test-index.html
 */
 gulp.task("test-js-injector", function () {                                    
-    return gulp.src("./Test/dest/dest/test-index.html")
-               .pipe(lazy.inject(gulp.src("./test/dest/*.js", {read: false})))
-               .pipe(gulp.dest("./Test/dest/dest"));
+    return gulp.src(config.testDest + "dest/test-index.html")
+               .pipe(lazy.inject(gulp.src(config.testDest + "*.js", {read: false})))
+               .pipe(gulp.dest(config.testDest + "dest/"));
 });
 
 /*
@@ -194,8 +194,8 @@ gulp.task("css-injector", function () {
 */
 gulp.task("test-css-injector", function () {
     return gulp.src("./Test/dest/dest/test-index.html")
-               .pipe(lazy.inject(gulp.src("./Test/dest/dest/test-main.css", {read: false})))
-               .pipe(gulp.dest("./Test/dest/dest/"));
+               .pipe(lazy.inject(gulp.src(config.testDest + "dest/test-main.css", {read: false})))
+               .pipe(gulp.dest(config.testDest + "dest/"));
 });
 
 /*
@@ -328,10 +328,10 @@ gulp.task("minify-html", function () {
 * * * This task is used for testing. To check whether a simple html page has been minified or not
 */
 gulp.task("test-minify-html", function () {
-    return gulp.src("./Test/lib/test.html")
+    return gulp.src(config.testLib + "test.html")
                .pipe(lazy.minifyHtml({conditionals: true, spare:true}))
                .pipe(lazy.rename({suffix: ".min"}))
-               .pipe(gulp.dest("./Test/dest/"));
+               .pipe(gulp.dest(config.testDest));
 });
 
 
@@ -347,9 +347,9 @@ gulp.task("images", function () {
 * * * This tasks is used for testing. To check whether a simple image has been minified or not
 */
 gulp.task("test-images", function () {
-    return gulp.src("./Test/lib/img.jpg")
+    return gulp.src(config.testLib + "img.jpg")
                .pipe(lazy.imagemin({optimizationLevel: 5}))
-               .pipe(gulp.dest("./Test/dest/"));
+               .pipe(gulp.dest(config.testDest));
 });
 
 
@@ -376,10 +376,10 @@ gulp.task("template-cache", function () {
 * * * This task is used for testing.
 */
 gulp.task("test-template-cache", function () {
-    return gulp.src("./Test/lib/tmpl.html")
+    return gulp.src(config.testLib + "tmpl.html")
                .pipe(lazy.minifyHtml({empty: true}))
                .pipe(lazy.angularTemplatecache())
-               .pipe(gulp.dest("./Test/dest/"));
+               .pipe(gulp.dest(config.testDest));
 });
 
 
@@ -396,10 +396,10 @@ gulp.task("minify-css", function () {
 * * * This task is used for testing. To check whether a simple css file has been minified or not
 */
 gulp.task("test-minify-css", function () {
-    return gulp.src("./Test/lib/test.css")
+    return gulp.src(config.testLib + "test.css")
                .pipe(lazy.minifyCss({keepBreaks: false}))
                .pipe(lazy.rename({suffix: ".min"}))
-               .pipe(gulp.dest("./Test/dest/"));
+               .pipe(gulp.dest(config.testDest));
 });
 
 
@@ -417,11 +417,11 @@ gulp.task("minify-js", function () {
 * * * This task is used for testing. To check whether a simple css file has been minified or not
 */
 gulp.task("test-minify-js", function () {
-    return gulp.src("./Test/lib/test.js")
+    return gulp.src(config.testLib + "test.js")
                .pipe(lazy.stripDebug())
                .pipe(lazy.uglify())
                .pipe(lazy.rename({suffix: ".min"}))
-               .pipe(gulp.dest("./Test/dest/"));
+               .pipe(gulp.dest(config.testDest));
 });
 
 
@@ -432,6 +432,14 @@ gulp.task("dependency-fixer", function () {
   return gulp.src(config.alljs)
              .pipe(lazy.ngAnnotate()) 
              .pipe(gulp.dest(config.dev));
+});
+/*
+* * * This task is used for testing. It should inject angular dependecies 
+*/
+gulp.task("test-dependency-fixer", function () {
+  return gulp.src(config.testLib + "dependency-test.js")
+             .pipe(lazy.ngAnnotate()) 
+             .pipe(gulp.dest(config.testDest));
 });
 
 
